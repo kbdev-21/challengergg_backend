0c19c8d3abb64317d1235e698076e64793ea1336
DKB21 <88276913+DKB21@users.noreply.github.com>
2025-07-18 11:31:14 +0700

    add feature: add cors config

--- src/main/kotlin/com/example/challengergg/controller/PlayerController.kt
+++ src/main/kotlin/com/example/challengergg/controller/PlayerController.kt
@@ -2,6 +2,7 @@ package com.example.challengergg.controller
 
 import com.example.challengergg.dto.PlayerDto
 import com.example.challengergg.service.PlayerService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
